53b54d6ba595f02a61e74d8f9aff86e57c739b18
goodluck8889 <benjaminanton8889@gmail.com>
2013-10-20 10:13:34 -0700

    commit key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Loginapp::Application.config.secret_token = 'fb8236f0f90949b743c60e91d87901e6640a5894cfec58dc97d1b85f61b5c57f5c1f3d63845aad4487225c04c1c332a7e2bdb3a367809796617bf2ff1c0e555e'
+Loginapp::Application.config.secret_token = '0ab8b71e5f1c2f09a337dcf2f712d1e621265b50cc4301e5b6bec2290b595776ba372dd9130d5f81e5b721aa2d3a7b15df8ea93c0dad20c5ecb9d19b8836d161'
